3a2a08d0d954280efd748e985f2f106737789b84
Kris Bethel <krisbethel@Kriss-MacBook-Air.local>
2025-09-25 13:43:01 +0100

    Update

--- src/services/mailerService.js
+++ src/services/mailerService.js
@@ -2,8 +2,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT ,
-  secure: false,
+  port: Number(process.env.EMAIL_PORT),
+  secure: Number(process.env.EMAIL_PORT) === 465, // true for port 465, false otherwise
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
